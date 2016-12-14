59506e92328dd8fcaf28ff2e52142c1e1d00a1bb
Greg Hurrell <greg@hurrell.net>
2016-12-14 15:46:35 -0600

    Add relay tutorial

---
+++ hello-world/index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const { graphql, buildSchema } = require('graphql')
+
+const schema = buildSchema(`
+    type Video {
+        id: ID,
+        title: String,
+        duration: Int,
+        watched: Boolean,
+    }
+
+  type Query {
+    video: Video
+  }
+
+  type Schema {
+    query: Query
+  }
+`)
+
+const resolvers = {
+  video: () => ({
+id: '1',
+title: 'Foo',
+duration: 180,
+watched: true,
+  }),
+}
+
+const query = `
+query myFirstQuery {
+  video {
+    id,
+    title,
+    duration,
+    watched,
+}
+`;
+
+graphql(schema, query, resolvers)
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error))
